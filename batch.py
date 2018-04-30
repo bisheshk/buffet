b799ec5fdc0d9976b04f0c371313aeb54bb485b5
Bishesh Khadka <bkhadka@mit.edu>
2018-04-30 13:31:58 -0400

    XD

--- batch.py
+++ batch.py
@@ -11,17 +11,31 @@ with open('config.json') as data_file:
 
 API_KEY = data["KEY"]
 
+working_dict = {}
+
+mem_limit = 5000
+
+def initialize_memory(ticker):
+	for ticker in tickers:
+		working_dict[ticker] = []
+	return working_dict
+
+
 def batch_request(tickers):
+	# returns json obj 
 	stringified_tickers = ",".join(tickers)
 	print tickers 
 	r = requests.get('https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTES&symbols=' + stringified_tickers+'&apikey=' + API_KEY)
-	json_batch = j.json()
-	print json_batch
+	json_batch = r.json()
 	return json_batch
 
 def add_to_db(symbol, price, volume, timestamp):
-
+	print symbol
+	print price
+	print volume
+	print timestamp
 	# add code to commit data
+	working_dict[symbol].append((price, volume, timestamp))
 	return 
 
 
@@ -32,7 +46,9 @@ def unpack_batch(b_request):
 		price = quote_info['2. price']
 		volume = quote_info['3. volume']
 		timestamp = quote_info['4. timestamp']
-
 		add_to_db(symbol, price, volume, timestamp)
 
-batchRequest(['NVDA', 'MSFT'])
+ob = batch_request(['NVDA', 'MSFT'])
+unpack_batch(ob)
+
+
